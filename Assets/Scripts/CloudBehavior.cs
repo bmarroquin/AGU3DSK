f16234d679d2a3cd0eaa8d2227a9999eb84c3283
Brian Marroquin <brian.marroquin.94@my.csun.edu>
2014-05-03 16:03:55 -0700

    Cloud Behavior and model Transferred
    With a cloud you can set its orbit speed, where it orbits, how fast it
    rotates and distance from center. Starting position doesnt matter
    because it is move on initialization.

---
+++ Assets/Scripts/CloudBehavior.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudBehavior : MonoBehaviour {
+	public float rotationSpeed = 1f;
+	public Vector3 orbitLocation = new Vector3(1000,30,1000);
+	public float orbitSpeed = 1f;
+	public int distanceFromCenter = 20;
+	// Use this for initialization
+	void Start () {
+		this.transform.position = new Vector3 (orbitLocation.x - distanceFromCenter, orbitLocation.y, orbitLocation.z);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Rotate();
+	}
+	
+	private void Rotate(){
+		this.transform.Rotate(Vector3.up * this.rotationSpeed);
+		this.transform.RotateAround(orbitLocation, Vector3.up, this.orbitSpeed);
+	}
+}
