4ea9c38cf6882f098600b7d1eb45131eb8211802
Brian Marroquin <brian.marroquin.94@my.csun.edu>
2014-05-03 23:14:34 -0700

    Randomized cloud placement + create x many clouds.

--- Assets/Scripts/CloudBehavior.cs
+++ Assets/Scripts/CloudBehavior.cs
@@ -2,13 +2,29 @@
 using System.Collections;
 
 public class CloudBehavior : MonoBehaviour {
-	public float rotationSpeed = 1f;
-	public Vector3 orbitLocation = new Vector3(1000,30,1000);
-	public float orbitSpeed = 1f;
-	public int distanceFromCenter = 20;
+	public Vector2 rotationSpeedRange = new Vector2(1,4);
+	public Vector2 positionRange = new Vector2(30,481);
+	public Vector2 heightRange = new Vector2(40, 50);
+	public int scaleRandomizer = 2;
+	public Vector2 distnceFromCenterRange= new Vector2(10,30);
+	public Vector2 orbitSpeedRange = new Vector2(1, 5);
+	
+	private float rotationSpeed;
+	private Vector3 orbitLocation;
+	private float orbitSpeed;
+	
+	
 	// Use this for initialization
 	void Start () {
-		this.transform.position = new Vector3 (orbitLocation.x - distanceFromCenter, orbitLocation.y, orbitLocation.z);
+		rotationSpeed = Random.Range (rotationSpeedRange.x, rotationSpeedRange.y);
+		orbitSpeed = Random.Range(orbitSpeedRange.x, orbitSpeedRange.y);
+		float scale = Random.value *scaleRandomizer;
+		float x = Random.Range(positionRange.x, positionRange.y);
+		float y = Random.Range(heightRange.x, heightRange.y);
+		float z = Random.Range(positionRange.x, positionRange.y);
+		orbitLocation = new Vector3(x,y,z);
+		transform.position = new Vector3(x-Random.Range(distnceFromCenterRange.x, distnceFromCenterRange.y),y,z);
+		transform.localScale = transform.localScale*scale;
 	}
 	
 	// Update is called once per frame
---
+++ Assets/Scripts/CreatorOfHeavens.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreatorOfHeavens : MonoBehaviour {
+
+	public GameObject cloudPrefab;
+	public int cloudCount;
+	// Use this for initialization
+	void Start () {
+		for(int i = 0; i < cloudCount; i++){
+			Instantiate(cloudPrefab);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
