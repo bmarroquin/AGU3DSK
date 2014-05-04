9f54fafdcbf761ff112c4c42cee96b4be148364d
Brian Marroquin <brian.marroquin.94@my.csun.edu>
2014-05-03 17:01:58 -0700

    Camera Functionality.
    Swap through 5 cameras.

---
+++ Assets/Scripts/CameraBehavior.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBehavior : MonoBehaviour {
+	private Camera active;
+	public Camera world;
+	public Camera playerFP;
+	public Camera playerTP;
+	public Camera npFP;
+	public Camera npTP;
+	private int counter = 0;
+	// Use this for initialization
+	void Start () {
+		active = world;
+		active.gameObject.SetActive(true);
+		playerFP.gameObject.SetActive(false);
+		playerTP.gameObject.SetActive(false);
+		npFP.gameObject.SetActive(false);
+		npTP.gameObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("c")){
+			counter++;
+			active.gameObject.SetActive(false);
+			switch(counter%5){
+				case 0: active = world; break;
+				case 1: active = playerFP; break;
+				case 2: active = playerTP; break;
+				case 3: active = npFP; break;
+				case 4: active = npTP; break;
+			}
+			active.gameObject.SetActive(true);
+		}
+	}
+}
