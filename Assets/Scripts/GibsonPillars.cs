f7f3dcda66bc5f6eaf51bc79ad6f3481ec8593f7
Ursula Messick <ursula.messick.95@my.csun.edu>
2014-05-08 23:47:53 -0700

    Add in treasures, gibson, and GUI scripts. Added textures and created resource file
    New Textures:
    heightTexture.png
    gibson.png
    colorTexture.png
    
    New RAW:
    heightMap.raw
    
    New Scripts
    Treasure.cs - Controls treasure objects
    GUI_Window.cs - Infowindow
    GibsonPillars.cs - Display A grid of pillars
    
    Updated Scripts
    NOController.cs - only search for non-tagged treasures and (attempt) to handle collisions
    
    Scene Update:
    Added treasures. Attached GibsonPillars script to the scene.

---
+++ Assets/Scripts/GibsonPillars.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GibsonPillars : MonoBehaviour {
+
+	private Object pillarPrefab ;
+	private GameObject terrain;
+
+	// Use this for initialization
+	void Start () {
+		pillarPrefab = Resources.Load("pillar");
+		terrain =  GameObject.Find("Terrain");
+		int offset = 8;
+
+		for (int i = 0; i < 15; i++) {
+
+			for (int j = 0; j < 15; j++) {
+				float x = 50 + (offset * i);
+				float z = 50 + (offset * j);
+
+				//Grab the height and make sure pillar is above.
+				terrain =  GameObject.Find("Terrain");
+				float height = terrain.GetComponent<Terrain>().SampleHeight(new Vector3(x,0,z))+1.4f;
+				Instantiate (pillarPrefab, new Vector3 (x, height, z), Quaternion.identity);
+			}
+		}
+	}		
+}
+
---
+++ Assets/Scripts/Treasure.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Treasure : MonoBehaviour {
+	
+	public bool isTagged = false;
+	public string whoTagged = "";
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void Tag() {
+		isTagged = true;
+	}
+	
+	bool IsTagged() {
+		return isTagged;
+	}
+	
+}
