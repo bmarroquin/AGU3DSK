402e7fd88526642bf6f873b5102c3b21e803ef46
Brian Marroquin <brian.marroquin.94@my.csun.edu>
2014-05-08 21:19:10 -0700

    NpAgent Update
    Added a path class to generate a path. NpAgent movement is in. Not
    Character motor related.

---
+++ Assets/Scripts/NPController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPController : MonoBehaviour {
+	public GameObject navNodePrefab;
+	public float speed = 1f;
+	public float snapDistance = 1.5f;
+	Terrain terrain;
+	private Path mainPath;
+	Vector3 nextGoal;
+	
+	void Start () {
+//		navNodePrefab = Resources.Load<GameObject>("Prefabs/NanNode");
+		terrain = GameObject.FindObjectOfType<Terrain>();
+		GeneratePath();
+		nextGoal = mainPath.GetNextNode();
+		transform.LookAt(CalculateLookAtVector(nextGoal));
+	}
+	
+	void GeneratePath(){
+		ArrayList path = new ArrayList();
+		path.Add(new Vector3(10,terrain.SampleHeight(new Vector3(10,0,10)),10));
+		path.Add(new Vector3(20,terrain.SampleHeight(new Vector3(20,0,15)),15));	
+		path.Add(new Vector3(15,terrain.SampleHeight(new Vector3(15,0,20)),20));	
+		path.Add(new Vector3(100,terrain.SampleHeight(new Vector3(100,0,10)),10));	
+		path.Add(new Vector3(34,terrain.SampleHeight(new Vector3(34,0,25)),25));	
+		mainPath = new Path(path, navNodePrefab);
+	}
+	
+
+	// Update is called once per frame
+	void Update () {
+		if(Vector3.Distance(transform.position, nextGoal)<snapDistance){
+			nextGoal = mainPath.GetNextNode();
+			transform.LookAt(CalculateLookAtVector(nextGoal));
+		}
+		
+		Vector3 newPos= transform.position + transform.forward * speed * Time.deltaTime;
+		transform.position = new Vector3(newPos.x, terrain.SampleHeight(new Vector3(newPos.x,0, newPos.z)), newPos.z);
+	}
+	/// <summary>
+	/// Calculates the look at vector so the the body isn't rotated to weird angles and stuff.
+	/// </summary>
+	private Vector3 CalculateLookAtVector(Vector3 pos){
+		return new Vector3(pos.x, transform.position.y, pos.z);
+	}
+}
---
+++ Assets/Scripts/Path.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Path :MonoBehaviour{
+	//List of Vector3s for the Path
+	ArrayList nodes;
+	//List of GameObjects. Used to remove prefabs.
+	ArrayList navNodes;
+	
+	//The current node we are going towards.
+	int index;
+	
+	//If the path is in reverse mode. 
+	bool reverseMode = false;
+	
+	GameObject nodePrefab;
+	
+	public Path(ArrayList n, GameObject o){
+		nodes = n;
+		nodePrefab = o;
+		index = -1;
+		AddNodesToScene();
+	}
+	
+	void AddNodesToScene(){
+		print (nodes);
+		navNodes = new ArrayList();
+		foreach(Vector3 pos in nodes){
+			navNodes.Add (Instantiate(nodePrefab, pos, Quaternion.identity));
+		}
+	
+	}
+	
+	
+	//Handles conditions for Index increments.
+	void UpdateIndex(){
+		if(reverseMode){
+			index--;
+		}
+		else{
+			index++;
+		}
+		if(index >= nodes.Count){
+			reverseMode = !reverseMode;
+			index = nodes.Count - 2;
+		}
+		else if(index< 0){
+			reverseMode = !reverseMode;
+			index = 1;
+		}
+	}
+	
+	//Grabs the next node available.
+	public Vector3 GetNextNode(){
+		UpdateIndex();
+		return (Vector3)nodes[index];
+	}
+	
+}
